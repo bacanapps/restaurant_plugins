d47cb0037b0bca0f336ffe6e9307a0c0877e9b82
Arshad <arshad@chummun.com>
2014-03-14 18:02:01 +0400

    Added triptych first and triptych last regions

--- plugins/layouts/site_layout/site-layout.tpl.php
+++ plugins/layouts/site_layout/site-layout.tpl.php
@@ -25,7 +25,7 @@
   <?php endif; ?>
 
   <?php if ($content['navigation_left'] || $content['navigation_right']): ?>
-    <nav class="navbar navbar-inverse" id="navigation" role="navigation">
+    <nav class="navbar navbar-default" id="navigation" role="navigation">
       <div class="container">
         <div class="row">
           <div class="col-md-2">
@@ -45,10 +45,25 @@
     </div> <!-- /#main -->
   </div> <!-- /#main-wrapper -->
 
-  <header id="footer" class="header" role="footer">
+  <?php if ($content['triptych_first'] || $content['triptych_last']): ?>
+    <div id="triptych">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 triptych-first">
+            <?php print $content['triptych_first']; ?>
+          </div>
+          <div class="col-md-6 triptych-last">
+            <?php print $content['triptych_last']; ?>
+          </div>
+        </div>
+      </div>
+    </div>
+  <?php endif; ?>
+
+  <footer id="footer" class="header" role="footer">
     <div class="container">
       <div class="row">
-        <div class="col-md-4 column-1">
+        <div class="col-md-8 column-1">
           <div class="panel-panel-inner">
             <?php print $content['footer_col1']; ?>
           </div>
@@ -58,13 +73,8 @@
             <?php print $content['footer_col2']; ?>
           </div>
         </div>
-        <div class="col-md-4 column-3">
-          <div class="panel-panel-inner">
-            <?php print $content['footer_col3']; ?>
-          </div>
-        </div>
       </div>
     </div>
-  </header> <!-- /#header -->
+  </footer> <!-- /#footer -->
 
 </div> <!-- /.site-layout -->
