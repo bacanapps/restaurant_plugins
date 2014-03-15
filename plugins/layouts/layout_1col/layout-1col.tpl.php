a59d48da750bf0e687138b091d23428c204e395c
Arshad <arshad@chummun.com>
2014-03-15 17:44:17 +0400

    Updates for layout-1col

--- plugins/layouts/layout_1col/layout-1col.tpl.php
+++ plugins/layouts/layout_1col/layout-1col.tpl.php
@@ -13,50 +13,64 @@
 <div class="layout-1col clearfix <?php if (!empty($classes)) { print $classes; } ?><?php if (!empty($class)) { print $class; } ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
   
   <?php if ($content['banner']): ?>
-    <div id="banner" class="region">
-      <?php print $content['banner']; ?>
+    <div id="banner" class="panel-pane-region panel-region-full-width">
+      <div class="panel-region-inner">
+        <?php print $content['banner']; ?>
+      </div>
     </div> <!-- /#banner -->
   <?php endif; ?>
   
   <?php if ($content['contentmain1']): ?>
-    <div id="contentmain1" class="region">
+    <div id="contentmain1" class="panel-pane-region">
       <div class="container">
-        <?php print $content['contentmain1']; ?>
+        <div class="panel-region-inner">
+          <?php print $content['contentmain1']; ?>
+        </div>
       </div>
     </div> <!-- /#contentmain1 -->
   <?php endif; ?>
 
   <?php if ($content['full_width_1']): ?>
-    <div id="full-width-1" class="region">
-      <?php print $content['full_width_1']; ?>
+    <div id="full-width-1" class="panel-pane-region panel-region-full-width">
+      <div class="panel-region-inner">
+        <?php print $content['full_width_1']; ?>
+      </div>
     </div> <!-- /#full-width-1 -->
   <?php endif; ?>
 
   <?php if ($content['contentmain2']): ?>  
-    <div id="contentmain2" class="region">
+    <div id="contentmain2" class="panel-pane-region">
       <div class="container">
-        <?php print $content['contentmain2']; ?>
+        <div class="panel-region-inner">
+          <?php print $content['contentmain2']; ?>
+        </div>
       </div>
     </div> <!-- /#contentmain2 -->
   <?php endif; ?>
 
   <?php if ($content['full_width_2']): ?>
-    <div id="full-width-2" class="region">
-      <?php print $content['full_width_2']; ?>
+    <div id="full-width-2" class="panel-pane-region panel-region-full-width">
+      <div class="panel-region-inner">
+        <?php print $content['full_width_2']; ?>
+      </div>
     </div> <!-- /#full-width-2 -->
   <?php endif; ?>
 
   <?php if ($content['contentmain3']): ?>
-    <div id="contentmain3" class="region">
+    <div id="contentmain3" class="panel-pane-region">
       <div class="container">
-        <?php print $content['contentmain3']; ?>
+        <div class="panel-region-inner">
+          <?php print $content['contentmain3']; ?>
+        </div>
       </div>
     </div> <!-- /#contentmain3 -->
   <?php endif; ?>
 
   <?php if ($content['full_width_3']): ?>
-    <div id="full-width-3" class="region">
-      <?php print $content['full_width_3']; ?>
+    <div id="full-width-3" class="panel-pane-region panel-region-full-width">
+      <div class="panel-region-inner">
+        <?php print $content['full_width_3']; ?>
+      </div>
     </div> <!-- /#full-width-3 -->
   <?php endif; ?>
 
